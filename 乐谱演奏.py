46f264f0db161ba83793bb2c1a32c8002b8df54b
zhhony <zhhony@126.com>
2022-06-10 16:28:19 +0800

    优化注释

--- 乐谱演奏.py
+++ 乐谱演奏.py
@@ -18,12 +18,12 @@ def __voice(note: str, voice: int) -> None:
     note.stop()
 
 
-# 根据提供的路径获取乐谱并转化为pythonlist对象
+# 根据提供的路径获取乐谱
 def __MusicLoad(musicPath: Path) -> list:
     return json.load(open(musicPath))
 
 
-# 对输入的乐谱从0拍开始往下计数，编出每一个基本拍所要演奏的音符组，形成含有拍数的乐谱并返回
+# 对输入的乐谱从0(基本)拍开始往下计拍，返回含有(基本)拍数的乐谱
 def __TimeStamp(music: list) -> list:
     beat = 0  # 从0开始计算拍数
     notesWithBeat = []
@@ -36,13 +36,13 @@ def __TimeStamp(music: list) -> list:
     return musicScoreWithBeat
 
 
-# 返回一个装载乐谱的生成器,按顺序每next一次放出一个音符组
+# 返回一个装载乐谱的生成器,按顺序放出谱内的音符组
 def __MusicGenerator(music: list) -> list:
     for notes in music:
         yield notes
 
 
-# 后台同步进程，每过MUSIC_TIME_STAMP秒，往前推进一基本拍，并进行一次乐谱检测。如果监测到音符组，则对此音符组执行子进程。
+# 后台同步进程，每过MUSIC_TIME_STAMP秒，往前推进一基本拍，并与乐谱进行一次关联。如果关联到音符组，则对此音符组执行__addThread调用。
 def __DaemonSynchronizationThread(musicGeneratorA: Generator, musicGeneratorB: Generator, speed: int) -> None:
     MUSIC_MIN_TIME_STAMP = 60/speed/8  # 一基本拍的时间(基本拍：一个32分音符为一拍，区别于一般意义的拍)
     beat = 0
@@ -63,13 +63,13 @@ def __DaemonSynchronizationThread(musicGeneratorA: Generator, musicGeneratorB: G
         except StopIteration:
             errorcountB = 1
         finally:
-            if errorcountA + errorcountB == 2:
+            if errorcountA + errorcountB == 2:  # 两个谱子都放完的情况下退出此函数
                 break
             beat += 1
             sleep(MUSIC_MIN_TIME_STAMP)
 
 
-# 针对传递过来的音符组，建立演奏线程
+# 针对传递过来的音符组，建立演奏线程，调用__voice处理
 def __addThread(notes: list) -> None:
     for note in notes[0]:
         t = Thread(target=__voice, args=(note, notes[2]))
@@ -98,7 +98,7 @@ if __name__ == '__main__':
     musicGenerator = __MusicGenerator(musicWithBeat)
     chordGenerator = __MusicGenerator(chordWithBeat)
 
-    # 启动接口
+    # 预热接口
     warmUp = mp3play.load("D:\\WorkShop\\Python\\standard_pitch\\4E.mp3")
     warmUp.volume(0)
     warmUp.play()
@@ -110,16 +110,3 @@ if __name__ == '__main__':
 
 # python 乐谱演奏.py -m 紫竹调A轨.json -c 紫竹调B轨.json -s 96
 # python 乐谱演奏.py -m canonCLeft.json -c canonCRight.json -s 80
-
-
-# bytes = b'\xe5\x88\x9d\xe5\xa7\x8b\xe5\x8c\x96 MCI \xe6\x97\xb6\xe5\x8f\x91\xe7\x94\x9f\xe9\x97\xae\xe9\xa2\x98\xe3\x80\x82'
-# bytes.decode('utf8')
-
-# note = '4C'
-# path = Path('D:\\WorkShop\\Python\\standard_pitch')  # 88标准音位置
-# mp3Path = path / (note + '.mp3')
-#
-# note.volume(100)
-# note.play()
-# sleep(4)
-# note.stop()
